04373d7aea91d7e9660c1c040a363df628a4b76b
Sujith013 <sujith.2020@vitstudent.ac.in>
2021-04-21 12:43:29 +0530

    2nd commit

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+
+if(isset($_POST["submit"]))
+{
+$username=$_POST["username"];
+$password=$_POST["pass"];
+}
+
+$host="localhost";
+$dbusername="root";
+$dbpassword="Jithtn@0012";
+$dbname="BlackVilles";
+
+$conn=new mysqli($host,$dbusername,$dbpassword,$dbname,"3307");
+
+if ($conn->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$check="SELECT * from user_details where username='".$username."' AND password='".$password."' limit 1";
+$result=mysqli_query($conn,$check);
+if(mysqli_num_rows($result)==1)
+{
+    header("Location:https://www.youtube.com/watch?v=aIsu9SPcGbU");
+
+}
+
+?>
+
+<script>
+    alert('Wrong Username or Password!!...Please check the credentials entered');
+    window.location.href = "http://localhost:7882/DBMS-project/login.html" ;
+</script>";
---
+++ signup.php
@@ -0,0 +1,52 @@
+
+<?php
+
+if(isset($_POST["submit"]))
+{
+$username=$_POST["username"];
+$password=$_POST["pass"];
+$email=$_POST["email"];
+$phone=$_POST["phone"];
+$addressl1=$_POST["addressl1"];
+$addressl2=$_POST["addressl2"];
+$city=$_POST["city"];
+$pincode=$_POST["pincode"];
+$DOB=$_POST["Date"];
+}
+
+$host="localhost";
+$dbusername="root";
+$dbpassword="Jithtn@0012";
+$dbname="BlackVilles";
+
+$conn=new mysqli($host,$dbusername,$dbpassword,$dbname,"3307");
+
+if ($conn->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+$check="SELECT * from user_details where username='".$username."' OR email='".$email."' OR phone='".$phone."'  limit 1";
+$result=mysqli_query($conn,$check);
+if(mysqli_num_rows($result)!=1)
+{
+$insert="INSERT INTO user_details(username,password,email,phone,addressl1,addressl2,city,pincode,date) VALUES('$username','$password','$email','$phone','$addressl1','$addressl2','$city','$pincode','$DOB')";
+$run=mysqli_query($conn,$insert);
+
+if($run)
+{
+    if($insert)
+    {
+        header("Location: http://localhost:7882/DBMS-project/login.html");
+    }
+}
+}
+?>
+
+<script>
+    alert('Username, Email or Phone no already taken, Please enter a different one');
+    window.location.href = "http://localhost:7882/DBMS-project/SignUp.html" ;
+</script>";
+
+    
+
+
