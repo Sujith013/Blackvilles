5ad3b0eb3960b6e56be2c1fa0af5c3bc8ae48b99
Sujith M <72350770+Sujith013@users.noreply.github.com>
2022-11-13 22:07:32 +0530

    Update appointment.php

--- appointment.php
+++ appointment.php
@@ -16,7 +16,7 @@ $comments=$_POST["comments"];
 
 $host="localhost";
 $dbusername="root";
-$dbpassword="Jithtn@0012";
+$dbpassword="";
 $dbname="BlackVilles";
 
 $conn=new mysqli($host,$dbusername,$dbpassword,$dbname,"3307");
